f33049f05382f52a6823898eb3b95678a5956ec4
Morgan Krupp <morganrkrupp@lewisu.edu>
2024-03-05 18:10:27 -0500

    updated

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 const cors = require("cors")
 
 var url = require('url');
